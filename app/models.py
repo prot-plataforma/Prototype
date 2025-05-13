6287ee03d71a0a7dcc3ca365e7473399ce7eb09c
Thxgirl4 <anajuliasilva3304@gmail.com>
2025-05-13 11:24:31 -0300

    update: models.py

--- app/models.py
+++ app/models.py
@@ -2,14 +2,19 @@ from db import db
 from sqlalchemy import Column, Integer, String
 from flask_login import UserMixin
 
+
+
 class User(UserMixin, db.Model):
     __tablename__ = 'usuarios'
 
     id = db.Column(db.Integer, primary_key=True )
 
-    nm = db.Column(db.String(50), unique=True)
-    psswrd = db.Column(db.String())
+    nm = db.Column(db.String(50), unique=True, nullable=False)
+
+    psswrd = db.Column(db.String(), nullable=False)
 
     email = db.Column(db.String(120), unique=True)
 
     cred = db.Column(db.String(11), unique=True)
+
+
